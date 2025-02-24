dc693a3fad638bcd9da676a57a87a8711ed24095
Snhvn <snhvnnguyen@gmail.com>
2025-02-24 14:51:33 +0700

    Update python2-vi.py

--- python2-vi.py
+++ python2-vi.py
@@ -260,7 +260,7 @@ async def create_server_task(interaction):
     
     try:
         container_id = subprocess.check_output([
-            "docker", "run", "-itd", "--privileged", "--cap-add=ALL", image
+            "docker", "run", "-itd", image
         ]).strip().decode('utf-8')
     except subprocess.CalledProcessError as e:
         await interaction.followup.send(embed=discord.Embed(description=f"Error creating Docker container: {e}", color=0xff0000))
