7ae0b79aaad0bc29dadeaeddb4106df4214430a9
Snhvn <snhvnnguyen@gmail.com>
2025-02-24 14:12:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ sudo apt install -y python3-pip docker.io
 echo Installed successfully/Đã cài đặt thành công
 
 echo "Writing Dockerfile-Ubuntu.../Viết Dockerfile-Ubuntu..."
-wget https://github.com/katy-the-kat/discord-vps-creator/raw/refs/heads/main/Dockerfile1
+wget https://github.com/Snhvn/discord-vps-creator-vn-by-Snipavn-katy/raw/refs/heads/main/Dockerfile1
 echo "Writing Dockerfile-Debian.../Viết Dockerfile-Debian..."
 wget https://github.com/katy-the-kat/discord-vps-creator/raw/refs/heads/main/Dockerfile2
 
