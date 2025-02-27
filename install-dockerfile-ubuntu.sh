1b54de7f7ff0f7626e47f815d8414a99adfa8949
Snhvn <snhvnnguyen@gmail.com>
2025-02-27 12:21:45 +0700

    Update install-dockerfile-ubuntu.sh

--- install-dockerfile-ubuntu.sh
+++ install-dockerfile-ubuntu.sh
@@ -4,7 +4,4 @@ FROM ubuntu:22.04
 RUN apt update
 RUN apt install -y tmate
 RUN apt install -y wget
-RUN wget -O ngrok.tgz "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz" && \
-tar -xf ngrok.tgz && \
-rm -rf ngrok.tgz 
 EOF
