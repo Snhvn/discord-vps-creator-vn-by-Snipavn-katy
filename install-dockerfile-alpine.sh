a1dc5ee1d3e71bcb3e74f0d1800ca53872a9f5ed
Snhvn <snhvnnguyen@gmail.com>
2025-03-01 16:57:17 +0700

    Update install-dockerfile-alpine.sh

--- install-dockerfile-alpine.sh
+++ install-dockerfile-alpine.sh
@@ -1,36 +1,9 @@
 cat <<EOF > Dockerfile3
 FROM alpine:3.19
-    
-RUN apk add --no-cache --virtual build-dependencies \
-        build-base \
-        ca-certificates \
-        bash \
-        wget \
-        git \
-        openssh \
-        libc6-compat \
-        automake \
-        autoconf \
-        zlib-dev \
-        libevent-dev \
-        msgpack-c-dev \
-        ncurses-dev \
-        libexecinfo-dev \
-        libssh-dev \
-        libc6-compat \
-        libssh \
-        msgpack-c \
-        ncurses-libs \
-        libevent
 
-RUN mkdir /src && \
-    git clone https://github.com/tmate-io/tmate-slave.git /src/tmate-server && \
-    cd /src/tmate-server && \
-    git apply /backtrace.patch && \
-    ./autogen.sh && \
-    ./configure CFLAGS="-D_GNU_SOURCE" && \
-    make -j && \
-    cp tmate-slave /bin/tmate-slave && \
-    apk del --no-cache build-dependencies
+RUN apk update && \
+    echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
+    apk add --no-cache tmate && \
+    apk add sudo neofetch curl wget
     
 EOF
