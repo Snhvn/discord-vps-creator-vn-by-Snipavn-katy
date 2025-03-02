0005a1665ddf984f905b60d006ce23d4b7ec9edd
Snhvn <snhvnnguyen@gmail.com>
2025-03-02 14:56:16 +0700

    Update install-dockerfile-fedora.sh

--- install-dockerfile-fedora.sh
+++ install-dockerfile-fedora.sh
@@ -1,9 +1,8 @@
 cat <<EOF > Dockerfile4
 FROM fedora
-MAINTAINER http://fedoraproject.org/wiki/Cloud
 
 RUN dnf update -y && \
     dnf install tmate -y && \
     dnf install -y wget fastfetch curl git systemctl sudo
 
-    EOF
+EOF
