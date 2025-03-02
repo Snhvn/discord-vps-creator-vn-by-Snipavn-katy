3788d22ffc814e507041aa991c038bfb57848e44
Snhvn <snhvnnguyen@gmail.com>
2025-03-02 15:12:58 +0700

    Update install-dockerfile.sh

--- install-dockerfile.sh
+++ install-dockerfile.sh
@@ -1,1 +1,9 @@
-
+echo "Code By SNIPA VN | https://youtube.com/@snipavn205
+echo "Writing Dockerfile-Ubuntu.../Viết Dockerfile-Ubuntu"
+wget https://raw.githubusercontent.com/Snhvn/discord-vps-creator-vn-by-Snipavn-katy/refs/heads/main/dockerfile/install-dockerfile-ubuntu.sh && chmod +x install-dockerfile-ubuntu.sh && sudo ./install-dockerfile-ubuntu.sh
+echo "Writing Dockerfile-Debian.../Viết Dockerfile-Debian"
+wget https://raw.githubusercontent.com/Snhvn/discord-vps-creator-vn-by-Snipavn-katy/refs/heads/main/dockerfile/install-dockerfile-debian.sh && chmod +x install-dockerfile-debian.sh && sudo ./install-dockerfile-debian.sh
+echo "Writing Dockerfile-Alpine.../Viết Dockerfile-Alpine"
+wget https://raw.githubusercontent.com/Snhvn/discord-vps-creator-vn-by-Snipavn-katy/refs/heads/main/dockerfile/install-dockerfile-alpine.sh && chmod +x install-dockerfile-alpine.sh && sudo ./install-dockerfile-alpine.sh
+echo "Writing Dockerfile-Fedora.../Viết Dockerfile-Fedora"
+wget https://raw.githubusercontent.com/Snhvn/discord-vps-creator-vn-by-Snipavn-katy/refs/heads/main/dockerfile/install-dockerfile-fedora.sh && chmod +x install-dockerfile-fedora.sh && sudo ./install-dockerfile-fedora.sh
