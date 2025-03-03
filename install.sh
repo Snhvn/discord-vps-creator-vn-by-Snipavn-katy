892f53c3185615b640e091430e4f9842185a5605
Snhvn <snhvnnguyen@gmail.com>
2025-03-03 12:51:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-INSTALL_FLAG="discord-bot-vps-creator"
+INSTALL_FLAG="/discord-bot-vps-creator/"
 echo " ██ ███    ███ ██    ██  ██████  ███████ ██    ██ ██   ██ ██  
 ██  ████  ████  ██  ██  ██    ██ ██      ██    ██ ██   ██  ██ 
 ██  ██ ████ ██   ████   ██    ██ █████   ██    ██ ███████  ██ 
@@ -28,10 +28,10 @@ echo "Writing Dockerfile.../Viết Dockerfile..."
 wget https://github.com/Snhvn/discord-vps-creator-vn-by-Snipavn-katy/raw/refs/heads/main/install-dockerfile.sh && chmod +x install-dockerfile.sh && sudo ./install-dockerfile.sh && rm install-dockerfile.sh
 echo Made successfully - Building Docker image.
 echo "Building Docker Image/Xây dựng hình ảnh Docker"
-sudo docker build -t ubuntu-22.04-with-tmate -f Dockerfile1 . && sudo docker build -t debian-with-tmate -f Dockerfile2 . && sudo docker build -t alpine-with-tmate -f Dockerfile3 . && sudo docker build -t fedora-with-tmate -f Dockerfile4 . && sudo docker build -t openwrt-with-tmate -f Dockerfile5 .
+sudo docker build -t ubuntu-22.04-with-tmate -f Dockerfile1 . && sudo docker build -t debian-with-tmate -f Dockerfile2 . && sudo docker build -t alpine-with-tmate -f Dockerfile3 . && sudo docker build -t fedora-with-tmate -f Dockerfile4 . #&& sudo docker build -t openwrt-with-tmate -f Dockerfile5 .
 echo Built successfully/Xây dựng thành công
 echo "Downloading main.py from the GitHub repository.../Đang tải xuống main.py từ kho lưu trữ GitHub/Snhvn..."
-echo -e "Enter "1-en" (English) (Ubuntu-22.04) or Enter "2-en" (English) (Full commands and add debian 12)/Nhập "1-vi" (Tiếng Việt) (Ubuntu-22.04) hoặc Nhập "2-vi" (Tiếng Việt) (Đầy đủ các lệnh và thêm debian-12), Nhập "3-vi" (Tiếng Việt) (Đầy đủ các lệnh và thêm Alpine và Fedora,Openwrt) "
+echo -e "Enter "1-en" (English) (Ubuntu-22.04) or Enter "2-en" (English) (Full commands and add debian 12)/Nhập "1-vi" (Tiếng Việt) (Ubuntu-22.04) hoặc Nhập "2-vi" (Tiếng Việt) (Đầy đủ các lệnh và thêm debian-12), Nhập "3-vi" (Tiếng Việt) (Đầy đủ các lệnh và thêm Alpine và Fedora,..) "
 read pythonnumber
 wget -O main.py https://raw.githubusercontent.com/Snhvn/discord-vps-creator-vn-by-Snipavn-katy/refs/heads/main/python$pythonnumber.py
 echo Downloaded successfully/Đã tải xuống thành công
