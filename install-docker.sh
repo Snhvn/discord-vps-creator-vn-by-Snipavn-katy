e5fe3c0f68582c28211573323a5a2bc31b3119a4
Snhvn <snhvnnguyen@gmail.com>
2025-03-08 19:57:50 +0700

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -3,6 +3,7 @@ clear
 echo "Code By SNIPAVN"
 echo "Installing Docker/ Cài đặt Docker"
 echo "Wait 3s to install/ Chờ 3s để cài"
+sleep 3
 clear
 sudo apt install docker.io
 echo "Đã Xong tải docker"
