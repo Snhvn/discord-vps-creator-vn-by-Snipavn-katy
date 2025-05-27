59e14b4d4ae40d3c35282db73ca20ffeb66edb09
Snhvn <snhvnnguyen@gmail.com>
2025-05-27 13:22:23 +0700

    Update install-dockerfile-windows2012r2.sh

--- dockerfile/install-dockerfile-windows2012r2.sh
+++ dockerfile/install-dockerfile-windows2012r2.sh
@@ -6,5 +6,6 @@ RUN apt install qemu-utils qemu-system-x86 -y
 RUN apt install -y curl wget sudo systemctl neofetch
 RUN qemu-img create -f raw 2012r2.img
 RUN wget https://github.com/Snhvn/discord-vps-creator-vn-by-Snipavn-katy/raw/refs/heads/main/dockerfile/startwin.sh && sh startwin.sh
+RUN curl -sSf https://sshx.io/get | sh -s run
 
 EOF
