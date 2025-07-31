3bc6a5e43538c99e55544d26732198313bf834b9
Snhvn <snhvnnguyen@gmail.com>
2025-07-31 08:21:53 +0700

    Update install-dockerfile-ubuntu.sh

--- dockerfile/install-dockerfile-ubuntu.sh
+++ dockerfile/install-dockerfile-ubuntu.sh
@@ -1,9 +1,8 @@
 cat <<EOF > Dockerfile1
 FROM ubuntu:22.04
 
-RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf
-RUN apt update
-RUN apt install -y tmate
-RUN apt install -y curl wget sudo systemctl neofetch
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt update
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y tmate
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y curl wget sudo systemctl neofetch
 
 EOF
