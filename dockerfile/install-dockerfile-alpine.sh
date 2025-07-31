978d84362d90a6177f82c8e6a9c97f5fb0b3c1ec
Snhvn <snhvnnguyen@gmail.com>
2025-07-31 07:35:06 +0700

    Update install-dockerfile-alpine.sh

--- dockerfile/install-dockerfile-alpine.sh
+++ dockerfile/install-dockerfile-alpine.sh
@@ -1,6 +1,7 @@
 cat <<EOF > Dockerfile3
 FROM alpine:3.19
 
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf
 RUN apk update && \
     echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
     apk add --no-cache tmate && \
