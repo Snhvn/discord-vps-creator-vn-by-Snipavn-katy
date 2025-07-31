af21a2841ec75ad5fdfbf468cf8f4597e85b52ef
Snhvn <snhvnnguyen@gmail.com>
2025-07-31 08:24:50 +0700

    Update install-dockerfile-fedora.sh

--- dockerfile/install-dockerfile-fedora.sh
+++ dockerfile/install-dockerfile-fedora.sh
@@ -1,9 +1,8 @@
 cat <<EOF > Dockerfile4
 FROM fedora
 
-RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf
-RUN dnf update -y && \
-    dnf install tmate -y && \
-    dnf install -y wget fastfetch curl git systemctl sudo
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && dnf update -y 
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf dnf install tmate -y
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && dnf install -y wget fastfetch curl git systemctl sudo
 
 EOF
