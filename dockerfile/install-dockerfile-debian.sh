c11b209b931777404cb0f21ca40fb878b8cbd49b
Snhvn <snhvnnguyen@gmail.com>
2025-07-31 07:34:46 +0700

    Update install-dockerfile-debian.sh

--- dockerfile/install-dockerfile-debian.sh
+++ dockerfile/install-dockerfile-debian.sh
@@ -1,6 +1,7 @@
 cat <<EOF > Dockerfile2
 FROM debian:12
 
+echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf
 RUN apt update
 RUN apt install -y tmate
 RUN apt install -y curl wget sudo systemctl neofetch
