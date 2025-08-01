6dc6b0f0e8253650ceb592e694295d884ed7c220
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 12:11:31 +0700

    Update install-dockerfile-debian.sh

--- dockerfile/install-dockerfile-debian.sh
+++ dockerfile/install-dockerfile-debian.sh
@@ -4,6 +4,6 @@ FROM debian:12
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt update
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y tmate
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y curl wget sudo systemctl neofetch
-RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+RUN cd && echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
 EOF
