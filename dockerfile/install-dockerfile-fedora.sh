3ad796636365d0f998e67ebcdfb094a9310b0d81
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 12:00:18 +0700

    Update install-dockerfile-fedora.sh

--- dockerfile/install-dockerfile-fedora.sh
+++ dockerfile/install-dockerfile-fedora.sh
@@ -4,5 +4,6 @@ FROM fedora
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && dnf update -y 
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf dnf install tmate -y
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && dnf install -y wget fastfetch curl git systemctl sudo
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
 EOF
