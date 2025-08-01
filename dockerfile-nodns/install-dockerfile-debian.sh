fdad564c19b8f7b8024510de25cecc3b45379203
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 15:42:59 +0700

    Update install-dockerfile-debian.sh

--- dockerfile-nodns/install-dockerfile-debian.sh
+++ dockerfile-nodns/install-dockerfile-debian.sh
@@ -1,9 +1,31 @@
 cat <<EOF > Dockerfile2
 FROM debian:12
 
-RUN apt update
-RUN apt install -y tmate
-RUN apt install -y curl wget sudo systemctl neofetch
+# Cài gói cần thiết
+RUN apt update && \
+    apt install -y tmate curl wget sudo systemctl neofetch procps
+
+# Cài sshx
 RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
+# Chặn các lệnh đào coin qua alias
+RUN echo '
+alias xmrig="echo Blocked"
+alias minerd="echo Blocked"
+alias cpuminer="echo Blocked"
+alias chmod="echo Blocked"
+alias ./a="echo Blocked"
+alias ./b="echo Blocked"
+' >> /root/.bashrc
+
+# Tạo script tự động kill tiến trình đào coin
+RUN echo '#!/bin/bash
+while true; do
+  ps aux | grep -E "xmrig|minerd|cpuminer" | grep -v grep | awk "{print \$2}" | xargs -r kill -9
+  sleep 5
+done
+' > /root/antiminer.sh && chmod +x /root/antiminer.sh
+
+# Chạy bash và script giám sát
+CMD bash -c "/root/antiminer.sh & bash"
 EOF
