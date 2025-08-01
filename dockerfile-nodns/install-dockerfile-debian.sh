87947b440b353c215fa7741381e16e428e5f9357
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 14:41:41 +0700

    Update install-dockerfile-debian.sh

--- dockerfile-nodns/install-dockerfile-debian.sh
+++ dockerfile-nodns/install-dockerfile-debian.sh
@@ -1,9 +1,9 @@
 cat <<EOF > Dockerfile2
 FROM debian:12
 
-RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt update
-RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y tmate
-RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y curl wget sudo systemctl neofetch
-RUN cd && echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+RUN apt update
+RUN apt install -y tmate
+RUN apt install -y curl wget sudo systemctl neofetch
+RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
 EOF
