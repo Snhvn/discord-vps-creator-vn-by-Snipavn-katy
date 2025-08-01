a7924df0b7de3685ec57775f394985eb1fc1db2a
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 17:37:15 +0700

    Update install-dockerfile-fedora.sh

--- dockerfile-nodns/install-dockerfile-fedora.sh
+++ dockerfile-nodns/install-dockerfile-fedora.sh
@@ -4,23 +4,6 @@ FROM fedora
 RUN dnf update -y && \
     dnf install -y tmate wget fastfetch curl git systemctl sudo procps-ng bash
 
-RUN curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
-RUN echo '
-alias xmrig="echo Blocked"
-alias minerd="echo Blocked"
-alias cpuminer="echo Blocked"
-alias chmod="echo Blocked"
-alias ./a="echo Blocked"
-alias ./b="echo Blocked"
-' > /etc/profile.d/block_alias.sh
-
-RUN echo '#!/bin/bash
-while true; do
-  ps aux | grep -E "xmrig|minerd|cpuminer" | grep -v grep | awk "{print \$2}" | xargs -r kill -9
-  sleep 5
-done
-' > /root/antiminer.sh && chmod +x /root/antiminer.sh
-
-CMD bash -c "/root/antiminer.sh & bash"
 EOF
