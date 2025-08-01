ed53a091a7cbaede53f1f63a888fe9a91598c96c
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 17:16:30 +0700

    Update install-dockerfile-alpine.sh

--- dockerfile-dns/install-dockerfile-alpine.sh
+++ dockerfile-dns/install-dockerfile-alpine.sh
@@ -1,10 +1,10 @@
+cat <<EOF > Dockerfile1
 FROM ubuntu:22.04
 
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt update
-RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y tmate
-RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y curl wget sudo systemctl neofetch procps
+RUN apt install -y tmate curl wget sudo systemctl neofetch procps
 
-RUN cd && echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
 RUN echo '
 alias xmrig="echo Blocked"
@@ -23,3 +23,4 @@ done
 ' > /root/antiminer.sh && chmod +x /root/antiminer.sh
 
 CMD bash -c "/root/antiminer.sh & bash"
+EOF
