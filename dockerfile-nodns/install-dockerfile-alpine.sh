6e015c50fa3546419fbf567ce63ce82bccc33050
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 17:39:56 +0700

    Update install-dockerfile-alpine.sh

--- dockerfile-nodns/install-dockerfile-alpine.sh
+++ dockerfile-nodns/install-dockerfile-alpine.sh
@@ -5,23 +5,6 @@ RUN apk update && \
     echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
     apk add --no-cache tmate sudo neofetch curl wget procps bash
 
-RUN curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
-RUN echo '
-alias xmrig="echo Blocked"
-alias minerd="echo Blocked"
-alias cpuminer="echo Blocked"
-alias chmod="echo Blocked"
-alias ./a="echo Blocked"
-alias ./b="echo Blocked"
-' > /etc/profile.d/block_alias.sh
-
-RUN echo '#!/bin/sh
-while true; do
-  ps aux | grep -E "xmrig|minerd|cpuminer" | grep -v grep | awk "{print \$1}" | xargs -r kill
-  sleep 5
-done
-' > /root/antiminer.sh && chmod +x /root/antiminer.sh
-
-CMD sh -c "/root/antiminer.sh & bash"
 EOF
