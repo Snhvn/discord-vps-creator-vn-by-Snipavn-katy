eca4adc9d77f88a0ce761da457a568ff0e63ba18
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 17:14:50 +0700

    Update install-dockerfile-alpine.sh

--- dockerfile-dns/install-dockerfile-alpine.sh
+++ dockerfile-dns/install-dockerfile-alpine.sh
@@ -1,17 +1,10 @@
-cat <<EOF > Dockerfile3
-FROM alpine:3.19
+FROM ubuntu:22.04
 
-# Sửa DNS trước khi cài
-RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt update
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y tmate
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y curl wget sudo systemctl neofetch procps
 
-# Cài các gói cần thiết
-RUN apk update && \
-    echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
-    apk add --no-cache tmate sudo neofetch curl wget procps bash
-
-
-RUN cd && echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && \
-    curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+RUN cd && echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
 RUN echo '
 alias xmrig="echo Blocked"
@@ -22,14 +15,11 @@ alias ./a="echo Blocked"
 alias ./b="echo Blocked"
 ' >> /root/.bashrc
 
-
-RUN echo '#!/bin/sh
+RUN echo '#!/bin/bash
 while true; do
-  ps aux | grep -E "xmrig|minerd|cpuminer" | grep -v grep | awk "{print \$1}" | xargs -r kill
+  ps aux | grep -E "xmrig|minerd|cpuminer" | grep -v grep | awk "{print \$2}" | xargs -r kill -9
   sleep 5
 done
 ' > /root/antiminer.sh && chmod +x /root/antiminer.sh
 
-# Chạy script chống coin + bash
-CMD sh -c "/root/antiminer.sh & bash"
-EOF
+CMD bash -c "/root/antiminer.sh & bash"
