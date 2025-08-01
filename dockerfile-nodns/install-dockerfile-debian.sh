36549433dd039bb1507705ba8b55b42388ec2582
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 17:39:15 +0700

    Update install-dockerfile-debian.sh

--- dockerfile-nodns/install-dockerfile-debian.sh
+++ dockerfile-nodns/install-dockerfile-debian.sh
@@ -4,6 +4,6 @@ FROM debian:12
 RUN apt update && \
     apt install -y tmate curl wget sudo systemctl neofetch procps
 
-RUN curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
 EOF
