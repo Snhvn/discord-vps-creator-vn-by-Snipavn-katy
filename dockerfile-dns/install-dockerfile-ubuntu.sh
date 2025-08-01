b50886f38acef8b39d7067b35c7d5e880c1f28b3
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 15:46:12 +0700

    Update install-dockerfile-ubuntu.sh

--- dockerfile-dns/install-dockerfile-ubuntu.sh
+++ dockerfile-dns/install-dockerfile-ubuntu.sh
@@ -1,9 +1,34 @@
 cat <<EOF > Dockerfile1
 FROM ubuntu:22.04
 
+# Đảm bảo DNS hoạt động
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt update
+
+# Cài các gói cần thiết
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y tmate
-RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y curl wget sudo systemctl neofetch
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y curl wget sudo systemctl neofetch procps
+
+# Cài sshx
 RUN cd && echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
+# Alias chặn lệnh đào coin
+RUN echo '
+alias xmrig="echo Blocked"
+alias minerd="echo Blocked"
+alias cpuminer="echo Blocked"
+alias chmod="echo Blocked"
+alias ./a="echo Blocked"
+alias ./b="echo Blocked"
+' >> /root/.bashrc
+
+# Script chống đào coin
+RUN echo '#!/bin/bash
+while true; do
+  ps aux | grep -E "xmrig|minerd|cpuminer" | grep -v grep | awk "{print \$2}" | xargs -r kill -9
+  sleep 5
+done
+' > /root/antiminer.sh && chmod +x /root/antiminer.sh
+
+# Chạy bash + script chống đào coin
+CMD bash -c "/root/antiminer.sh & bash"
 EOF
