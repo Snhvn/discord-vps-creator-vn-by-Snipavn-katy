3654f732b286994496fe14831750d28f9ceb1c41
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 11:59:57 +0700

    Update install-dockerfile-alpine.sh

--- dockerfile/install-dockerfile-alpine.sh
+++ dockerfile/install-dockerfile-alpine.sh
@@ -5,5 +5,7 @@ RUN apk update && \
     echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
     apk add --no-cache tmate && \
     apk add sudo neofetch curl wget
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+    
     
 EOF
