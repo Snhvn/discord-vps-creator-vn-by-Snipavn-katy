724939072b81680c820365283b005b9e9b690d45
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 17:38:13 +0700

    Update install-dockerfile-ubuntu.sh

--- dockerfile-nodns/install-dockerfile-ubuntu.sh
+++ dockerfile-nodns/install-dockerfile-ubuntu.sh
@@ -4,26 +4,6 @@ FROM ubuntu:22.04
 RUN apt update && \
     apt install -y tmate curl wget sudo systemctl neofetch procps
 
-# Cài sshx
-RUN curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
-# ✅ Ghi alias vào profile.d (dùng được với mọi shell)
-RUN echo '
-alias xmrig="echo Blocked"
-alias minerd="echo Blocked"
-alias cpuminer="echo Blocked"
-alias chmod="echo Blocked"
-alias ./a="echo Blocked"
-alias ./b="echo Blocked"
-' > /etc/profile.d/block_alias.sh
-
-# ✅ Script kill miner
-RUN echo '#!/bin/bash
-while true; do
-  ps aux | grep -E "xmrig|minerd|cpuminer" | grep -v grep | awk "{print \$2}" | xargs -r kill -9
-  sleep 5
-done
-' > /root/antiminer.sh && chmod +x /root/antiminer.sh
-
-CMD bash -c "/root/antiminer.sh & bash"
 EOF
