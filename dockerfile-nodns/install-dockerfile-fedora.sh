feb0c915bfbe2b0f359e572f21a0627608200c69
Snhvn <snhvnnguyen@gmail.com>
2025-08-01 15:45:09 +0700

    Update install-dockerfile-fedora.sh

--- dockerfile-nodns/install-dockerfile-fedora.sh
+++ dockerfile-nodns/install-dockerfile-fedora.sh
@@ -1,10 +1,31 @@
 cat <<EOF > Dockerfile4
 FROM fedora
 
-RUN dnf update -y 
-RUN dnf install tmate -y
-RUN dnf install -y wget fastfetch curl git systemctl sudo
-RUN curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+# Cập nhật và cài đặt các gói cần thiết
+RUN dnf update -y && \
+    dnf install -y tmate wget fastfetch curl git systemctl sudo procps-ng bash
+
+# Cài sshx
 RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
+# Alias chặn lệnh đào coin
+RUN echo '
+alias xmrig="echo Blocked"
+alias minerd="echo Blocked"
+alias cpuminer="echo Blocked"
+alias chmod="echo Blocked"
+alias ./a="echo Blocked"
+alias ./b="echo Blocked"
+' >> /root/.bashrc
+
+# Script tự động kill tiến trình đào coin
+RUN echo '#!/bin/bash
+while true; do
+  ps aux | grep -E "xmrig|minerd|cpuminer" | grep -v grep | awk "{print \$2}" | xargs -r kill -9
+  sleep 5
+done
+' > /root/antiminer.sh && chmod +x /root/antiminer.sh
+
+# Chạy bash kèm theo script giám sát đào coin
+CMD bash -c "/root/antiminer.sh & bash"
 EOF
