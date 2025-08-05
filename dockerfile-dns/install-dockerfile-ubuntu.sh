f9ac6bc01409aebd079ea8d8075df3f10f3bb67e
Snhvn <snhvnnguyen@gmail.com>
2025-08-05 15:51:47 +0700

    Update install-dockerfile-ubuntu.sh

--- dockerfile-dns/install-dockerfile-ubuntu.sh
+++ dockerfile-dns/install-dockerfile-ubuntu.sh
@@ -2,8 +2,8 @@ cat <<EOF > Dockerfile1
 FROM ubuntu:22.04
 
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt update
-RUN apt install -y tmate curl wget sudo systemctl neofetch procps
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt install -y tmate curl wget sudo systemctl neofetch procps
 
-RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
 EOF
