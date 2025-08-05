3a9bd74441ce1f8379e0aa31cc40ab189437ddad
Snhvn <snhvnnguyen@gmail.com>
2025-08-05 15:49:30 +0700

    Update install-dockerfile-alpine.sh

--- dockerfile-dns/install-dockerfile-alpine.sh
+++ dockerfile-dns/install-dockerfile-alpine.sh
@@ -10,21 +10,4 @@ RUN apk update && \
 RUN cd && echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && \
     curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
-RUN echo '
-alias xmrig="echo Blocked"
-alias minerd="echo Blocked"
-alias cpuminer="echo Blocked"
-alias chmod="echo Blocked"
-alias ./a="echo Blocked"
-alias ./b="echo Blocked"
-' >> /root/.bashrc
-
-RUN echo '#!/bin/sh
-while true; do
-  ps aux | grep -E "xmrig|minerd|cpuminer" | grep -v grep | awk "{print \$1}" | xargs -r kill
-  sleep 5
-done
-' > /root/antiminer.sh && chmod +x /root/antiminer.sh
-
-CMD sh -c "/root/antiminer.sh & bash"
 EOF
