cc75be54b4742dac64cdc1f450dfea8fc0ff0293
Snhvn <snhvnnguyen@gmail.com>
2025-08-05 15:50:07 +0700

    Update install-dockerfile-debian.sh

--- dockerfile-dns/install-dockerfile-debian.sh
+++ dockerfile-dns/install-dockerfile-debian.sh
@@ -6,21 +6,4 @@ RUN apt install -y tmate curl wget sudo systemctl neofetch procps
 
 RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
-RUN echo '
-alias xmrig="echo Blocked"
-alias minerd="echo Blocked"
-alias cpuminer="echo Blocked"
-alias chmod="echo Blocked"
-alias ./a="echo Blocked"
-alias ./b="echo Blocked"
-' >> /root/.bashrc
-
-RUN echo '#!/bin/bash
-while true; do
-  ps aux | grep -E "xmrig|minerd|cpuminer" | grep -v grep | awk "{print \$2}" | xargs -r kill -9
-  sleep 5
-done
-' > /root/antiminer.sh && chmod +x /root/antiminer.sh
-
-CMD bash -c "/root/antiminer.sh & bash"
 EOF
