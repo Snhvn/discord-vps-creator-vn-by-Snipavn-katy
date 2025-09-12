580958b3cfd3e2066c757e9c4882dc6cdd86fb95
Snhvn <snhvnnguyen@gmail.com>
2025-09-12 16:50:43 +0700

    Update install-dockerfile-debian.sh

--- dockerfile-nodns/install-dockerfile-debian.sh
+++ dockerfile-nodns/install-dockerfile-debian.sh
@@ -2,13 +2,12 @@ cat <<EOF > Dockerfile2
 FROM debian:12
 
 RUN apt update && \
-    apt install -y tmate curl wget sudo systemctl neofetch procps systemd systemd-sysv dbus gnupg2 apt-transport-https ca-certificates software-properties-common net-tools dnsutils iputils-ping docker.io
+    apt install -y tmate curl wget sudo neofetch procps systemd systemd-sysv dbus gnupg2 apt-transport-https ca-certificates software-properties-common net-tools dnsutils iputils-ping docker.io
 
 RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
 RUN systemctl enable docker
 
 STOPSIGNAL SIGRTMIN+3
-CMD ["/sbin/init"]
 
 EOF
