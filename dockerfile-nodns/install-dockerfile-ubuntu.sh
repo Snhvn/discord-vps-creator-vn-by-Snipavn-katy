d30a68c903dde65cbdfa09846b3596669633a76e
Snhvn <snhvnnguyen@gmail.com>
2025-09-12 16:50:08 +0700

    Update install-dockerfile-ubuntu.sh

--- dockerfile-nodns/install-dockerfile-ubuntu.sh
+++ dockerfile-nodns/install-dockerfile-ubuntu.sh
@@ -2,7 +2,7 @@ cat <<EOF > Dockerfile1
 FROM ubuntu:22.04
 
 RUN apt update && \
-    apt install -y tmate curl wget sudo systemctl neofetch procps systemd systemd-sysv dbus gnupg2 apt-transport-https ca-certificates software-properties-common net-tools dnsutils iputils-ping docker.io
+    apt install -y tmate curl wget sudo neofetch procps systemd systemd-sysv dbus gnupg2 apt-transport-https ca-certificates software-properties-common net-tools dnsutils iputils-ping docker.io
 
 RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
