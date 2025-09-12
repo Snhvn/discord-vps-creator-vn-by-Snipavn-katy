33717c3197bf75b7c50956e8f600ccd7a1e7cc01
Snhvn <snhvnnguyen@gmail.com>
2025-09-12 16:52:59 +0700

    Update install-dockerfile-fedora.sh

--- dockerfile-dns/install-dockerfile-fedora.sh
+++ dockerfile-dns/install-dockerfile-fedora.sh
@@ -2,7 +2,7 @@ cat <<EOF > Dockerfile4
 FROM fedora
 
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && dnf update -y
-RUN dnf install -y tmate curl wget sudo systemctl fastfetch procps systemd systemd-sysv dbus gnupg2 apt-transport-https ca-certificates software-properties-common net-tools dnsutils iputils-ping docker.io
+RUN dnf install -y tmate curl wget sudo fastfetch procps systemd systemd-sysv dbus gnupg2 apt-transport-https ca-certificates software-properties-common net-tools dnsutils iputils-ping docker.io
 
 RUN cd && echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && \
     curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
@@ -10,6 +10,6 @@ RUN cd && echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf &&
 RUN systemctl enable docker
 
 STOPSIGNAL SIGRTMIN+3
-CMD ["/sbin/init"]
+
 
 EOF
