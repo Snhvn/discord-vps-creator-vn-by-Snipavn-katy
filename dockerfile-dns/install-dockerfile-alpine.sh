fee56a5e713234f6fbaca154c8712d0a482a383d
Snhvn <snhvnnguyen@gmail.com>
2025-09-12 16:29:34 +0700

    Update install-dockerfile-alpine.sh

--- dockerfile-dns/install-dockerfile-alpine.sh
+++ dockerfile-dns/install-dockerfile-alpine.sh
@@ -5,9 +5,9 @@ RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf
 
 RUN apk update && \
     echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
-    apk add --no-cache tmate curl wget sudo systemctl neofetch procps systemd systemd-sysv dbus gnupg2 apt-transport-https ca-certificates software-properties-common net-tools dnsutils iputils-ping
+    apk add --no-cache tmate curl wget sudo systemctl neofetch procps dbus gnupg2 apt-transport-https ca-certificates software-properties-common net-tools dnsutils iputils-ping docker openrc
     
-RUN systemctl enable docker
+RUN rc-update add docker default
 
 RUN cd && echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && \
     curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
