5936f1eded3a2c021c22015ea7a97b87f1aa4291
Snhvn <snhvnnguyen@gmail.com>
2025-09-12 16:24:13 +0700

    Update install-dockerfile-fedora.sh

--- dockerfile-dns/install-dockerfile-fedora.sh
+++ dockerfile-dns/install-dockerfile-fedora.sh
@@ -7,7 +7,7 @@ RUN dnf install -y tmate curl wget sudo systemctl fastfetch procps systemd syste
 RUN cd && echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && \
     curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
 
-RUN systemctl restart docker
+RUN systemctl enable docker
 
 STOPSIGNAL SIGRTMIN+3
 CMD ["/sbin/init"]
