000678c0245b4056a22646e3dea8183941da311d
Snhvn <snhvnnguyen@gmail.com>
2025-09-12 16:36:24 +0700

    Update install-dockerfile-alpine.sh

--- dockerfile-nodns/install-dockerfile-alpine.sh
+++ dockerfile-nodns/install-dockerfile-alpine.sh
@@ -3,8 +3,13 @@ FROM alpine:3.19
 
 RUN apk update && \
     echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
-    apk add --no-cache tmate sudo neofetch curl wget procps bash
-
+    apk add --no-cache tmate curl wget neofetch procps openrc docker
+    
 RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+
+RUN rc-update add docker default
+
+STOPSIGNAL SIGRTMIN+3
+CMD ["/sbin/init"]
 
 EOF
