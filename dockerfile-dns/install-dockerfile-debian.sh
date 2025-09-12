8e6e1f4391a1955bd3c644a4d7251d7f2722c052
Snhvn <snhvnnguyen@gmail.com>
2025-09-12 16:25:06 +0700

    Update install-dockerfile-debian.sh

--- dockerfile-dns/install-dockerfile-debian.sh
+++ dockerfile-dns/install-dockerfile-debian.sh
@@ -2,9 +2,11 @@ cat <<EOF > Dockerfile2
 FROM debian:12
 
 RUN echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > /etc/resolv.conf && apt update
-RUN apt install -y tmate curl wget sudo systemctl neofetch procps systemd systemd-sysv dbus gnupg2 apt-transport-https ca-certificates software-properties-common net-tools dnsutils iputils-ping
+RUN apt install -y tmate curl wget sudo systemctl neofetch procps systemd systemd-sysv dbus gnupg2 apt-transport-https ca-certificates software-properties-common net-tools dnsutils iputils-ping docker.io
 
 RUN cd && curl -sSf https://sshx.io/get | sh -s download && chmod +x /root/sshx
+
+RUN systemctl enable docker
 
 STOPSIGNAL SIGRTMIN+3
 CMD ["/sbin/init"]
